6676b2579e198cb65ccfa32981e6ea6f9b589f2f
tamongbo <tamongbo@gmail.com>
2024-05-17 13:57:45 +0900

    [feature] 30

--- Baseball-Test/test.cpp
+++ Baseball-Test/test.cpp
@@ -5,3 +5,8 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
 	Baseball game;
 	EXPECT_THROW(game.guess(string("12")), length_error);
 }
+
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
